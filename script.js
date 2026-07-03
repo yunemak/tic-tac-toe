ce3bed7dbbc43bc6c16d8e94e9de273ae973f105
yunemak <yeak558@gmail.com>
2026-07-03 12:42:55 +0300

    Create basic factory function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+let gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]];
+
+
+
+const user1 = createPlayer("Yunus", "X");
+const user2 = createPlayer("Computer", "Y");
+
+console.log(user1.name);
+console.log(user2.mark);
+
+// Functions
+function createPlayer(name, mark) {
+	return { name, mark };
+}
