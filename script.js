3578f1b577b3c57f7cbb8daad90af08cf9c32078
yunemak <yeak558@gmail.com>
2026-07-04 22:00:47 +0300

    Add basic board variables and factory functions

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-let Gameboard = [["", "", ""], ["", "", ""], ["", "", ""]];
-
-
+let Gameboard = createGameboard();
 
 const user1 = createPlayer("Yunus", "X");
 const user2 = createPlayer("Computer", "Y");
 
-console.log(user1.name);
-console.log(user2.mark);
 
 // Functions
 function createPlayer(name, mark) {
-	return { name, mark };
+	let score = 0;
+
+	const getScore = () => score;
+	const increaseScore = () => {
+		score++;
+	}; 
+	return { name, mark, getScore, increaseScore };
+}
+
+function createGameboard() {
+	let board = [
+		"", "", "",
+		"", "", "",
+		"", "", ""
+	];
+
+	const putMark = (pos, mark) => {
+		if (board[pos] != "")
+			return (1);
+		board[pos] = mark;
+		return (0);
+	};
+
+	const printBoard = () => {
+		for (let i = 0; i < 3; i++) {
+			console.log(`${board[0+ 3*i]}   |   ${board[1+ 3*i]}   |   ${board[2 + 3*i]}`);
+		}
+	};
+
+	const checkGameEnd = () => {
+		if (board[0] === board[1] && board[0] != "")
+	}
+	return {putMark, printBoard};
 }
