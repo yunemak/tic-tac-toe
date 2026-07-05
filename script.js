1b3b6e26b6c628e77d08e6e29bfffcfea31d6196
yunemak <yeak558@gmail.com>
2026-07-05 10:27:05 +0300

    Add win check on Gameboard object

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const user1 = createPlayer("Yunus", "X");
 const user2 = createPlayer("Computer", "Y");
 
 
+
+
 // Functions
 function createPlayer(name, mark) {
 	let score = 0;
@@ -35,8 +37,31 @@ function createGameboard() {
 		}
 	};
 
-	const checkGameEnd = () => {
-		if (board[0] === board[1] && board[0] != "")
+	const checkWin = () => {
+		// Check rows
+		for (let i = 0; i < 3; i++) {
+			if (board[0 + 3*i] === "X" && board[1 + 3*i] === "X" && board[2 + 3*i] === "X")
+				return ("X");
+			if (board[0 + 3*i] === "O" && board[1 + 3*i] === "O" && board[2 + 3*i] === "O")
+				return ("O");
+		}
+		// Check columns
+		for (let i = 0; i < 3; i++) {
+			if (board[i] === "X" && board[i + 3] === "X" && board[i + 6] === "X")
+				return ("X");
+			if (board[i] === "O" && board[i + 3] === "O" && board[i + 6] === "O")
+				return ("O");
+		}
+		// Check crosses
+		if (board[0] === "X" && board[4] === "X" && board[8] === "X")
+			return ("X");
+		if (board[0] === "O" && board[4] === "O" && board[8] === "O")
+			return ("O");
+		if (board[2] === "X" && board[4] === "X" && board[6] === "X")
+			return ("X");
+		if (board[2] === "O" && board[4] === "O" && board[6] === "O")
+			return ("O");
+		return ("");
 	}
-	return {putMark, printBoard};
+	return {putMark, printBoard, checkWin};
 }
